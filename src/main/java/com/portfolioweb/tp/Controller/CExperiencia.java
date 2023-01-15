ecdcf591cb11f3718a80c20ab2269a39cdf4b42d
TomasPensado <tomas080827@gmail.com>
2023-01-14 22:58:41 -0300

    CORS

--- src/main/java/com/portfolioweb/tp/Controller/CExperiencia.java
+++ src/main/java/com/portfolioweb/tp/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "https://portfolio-web-z5od.onrender.com")
+@CrossOrigin(origins = "https://portfoliowebtp.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/portfolioweb/tp/Controller/PersonaController.java
+++ src/main/java/com/portfolioweb/tp/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://portfolio-web-z5od.onrender.com")
+@CrossOrigin(origins = "https://portfoliowebtp.web.app/")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
