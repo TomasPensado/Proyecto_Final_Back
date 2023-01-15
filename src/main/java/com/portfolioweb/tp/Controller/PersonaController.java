b36a0afd215c404c53b3498b685793b50a5f1b9e
TomasPensado <tomas080827@gmail.com>
2023-01-14 22:52:23 -0300

    CORS

--- src/main/java/com/portfolioweb/tp/Controller/PersonaController.java
+++ src/main/java/com/portfolioweb/tp/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-web-z5od.onrender.com")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
